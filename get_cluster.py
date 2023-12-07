0cec8d6f74030a365eec9dcec51e89d7a3f1cb2e
RoundShee <roundsheep@qq.com>
2023-12-07 22:21:40 +0800

    准备开始markov

--- get_cluster.py
+++ get_cluster.py
@@ -1,40 +1,20 @@
 from sklearn.cluster import DBSCAN
 import numpy as np
 import matplotlib.pyplot as plt
-import xml.etree.ElementTree as ET
+from make_data import get_one_aeiou_xy_series, plot_aeiou_train_sample
 
-# 生成一些随机的二维数据点
-name = ['./project1-data/a.xml', './project1-data/e.xml', './project1-data/i.xml', './project1-data/o.xml', './project1-data/u.xml']
-values = []
-x_all = []
-y_all = []
 
-for n in name:
-    tree = ET.parse(n)
-    root = tree.getroot()
-    for example in root.findall('trainingExample'):
-        x, y = [], []
-        for dots in example.findall('coord'):
-            x.append(float(dots.get('x')))
-            y.append(float(dots.get('y')))
-            values.append([float(dots.get('x')), float(dots.get('y'))])
-            x_all.append(float(dots.get('x')))
-            y_all.append(float(dots.get('y')))
-        plt.subplot(1, 2, 1)
-        plt.scatter(x, y)
-        break
+start, end = 0, 1
+values = get_one_aeiou_xy_series(start, end, fs=-1)
 X = np.array(values)
 
 # 使用DBSCAN进行聚类
-dbscan = DBSCAN(eps=20, min_samples=1)
+dbscan = DBSCAN(eps=30, min_samples=1)
 labels = dbscan.fit_predict(X)
 
-# 数据长度
-print(len(x_all))
-# 看看图
+x_all, y_all = plot_aeiou_train_sample(start, end, fs=-1)
 colors = ['c', 'b', 'g', 'r', 'm', 'y', 'k', 'w']
 colors_series = []
-plt.subplot(1, 2, 2)
 for i in labels:
     colors_series.append(colors[int(i)%8])
 plt.scatter(x_all, y_all, c=colors_series)
--- make_data.py
+++ make_data.py
@@ -8,20 +8,6 @@ import pickle
               [ex1,ey1], .... [ux20, uy20]
             ]
 """
-name = ['./project1-data/a.xml',
-        './project1-data/e.xml',
-        './project1-data/i.xml',
-        './project1-data/o.xml',
-        './project1-data/u.xml']
-
-# for n in name:
-#     tree = ET.parse(n)
-#     root = tree.getroot()
-#     for example in root.findall('trainingExample'):
-#         x_raw, y_raw = [], []
-#         for dots in example.findall('coord'):
-#             x_raw.append(float(dots.get('x')))
-#             y_raw.append(float(dots.get('y')))
 
 
 def get_xy_from_xml(filename, i, fs):
@@ -30,30 +16,100 @@ def get_xy_from_xml(filename, i, fs):
     :param filename:
     :param i:
     :param fs:
-    :return:
+    :return: xy
     """
     tree = ET.parse(filename)
     root = tree.getroot()
     var_i = 0
-    x, y = [], []   # 即将return
+    xy = []   # 即将return
     for example in root.findall('trainingExample'):
         if var_i == i:  # 找到对应i
-            x_raw, y_raw = [], []   # 全部保存
+            if fs == -1:
+                for dots in example.findall('coord'):
+                    xy.append([float(dots.get('x')), float(dots.get('y'))])
+            else:
+                x_raw, y_raw = [], []   # 全部保存
+                for dots in example.findall('coord'):
+                    x_raw.append(float(dots.get('x')))
+                    y_raw.append(float(dots.get('y')))
+                for var_j in range(0,len(x_raw)//fs*fs,len(x_raw)//fs):
+                    xy.append([x_raw[var_j], y_raw[var_j]])
+            var_i += 1
+        else:
+            var_i += 1
+    return xy
+
+
+def get_one_aeiou_xy_series(start, end, step=1, fs=15):
+    """
+    获取aeiou顺序序列
+    :param start: 包含从0开始
+    :param end: 不包含
+    :param step: 步长
+    :param fs
+    :return: xy
+    """
+    name = ['./project1-data/a.xml',
+            './project1-data/e.xml',
+            './project1-data/i.xml',
+            './project1-data/o.xml',
+            './project1-data/u.xml']
+    xy = []     # 即将返回
+    for i in range(start, end, step):
+        for j in name:
+            xy += get_xy_from_xml(j, i, fs)
+    return xy
+
+
+def plot_data_from_xml(filename, i, fs):
+    """
+    返回用于plot的[x, y]
+    :param filename:
+    :param i:
+    :param fs:
+    :return: [x, y]
+    """
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    var_i = 0
+    x, y = [], []  # 即将return
+    for example in root.findall('trainingExample'):
+        if var_i == i:  # 找到对应i
+            x_raw, y_raw = [], []  # 全部保存
             for dots in example.findall('coord'):
                 x_raw.append(float(dots.get('x')))
                 y_raw.append(float(dots.get('y')))
-            for var_j in range(0,len(x_raw)//fs*fs,len(x_raw)//fs):
-                x.append(x_raw[var_j])
-                y.append(y_raw[var_j])
+            if fs == -1:
+                x += x_raw
+                y += y_raw
+            else:
+                for var_j in range(0, len(x_raw) // fs * fs, len(x_raw) // fs):
+                    x.append(x_raw[var_j])
+                    y.append(y_raw[var_j])
+            var_i += 1
         else:
             var_i += 1
-    return x, y
+    return [x, y]
 
 
-def get_one_aeiou_xy_series(start, end):
+def plot_aeiou_train_sample(start, end, step=1, fs=15):
     """
-
+    对应get_one_aeiou_xy_series不加以区分的输出序列
     :param start:
     :param end:
+    :param step:
+    :param fs
     :return:
     """
+    name = ['./project1-data/a.xml',
+            './project1-data/e.xml',
+            './project1-data/i.xml',
+            './project1-data/o.xml',
+            './project1-data/u.xml']
+    x, y = [], []  # 即将返回
+    for i in range(start, end, step):
+        for j in name:
+            x_i, y_i = plot_data_from_xml(j, i, fs)
+            x += x_i
+            y += y_i
+    return [x, y]
